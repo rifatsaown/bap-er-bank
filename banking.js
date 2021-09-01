b9aa6c21e167a1978e07d7a98de69448a81d270d
rifatsaown <rifatsaown0@gmail.com>
2021-09-01 18:05:13 +0600

    reformd

--- banking.js
+++ banking.js
@@ -1,34 +1,45 @@
-function getInputValue(inputId){
+function getInputValue(inputId) {
     const input = document.getElementById(inputId);
     const inputNumber = parseFloat(input.value);
     input.value = '';
     return inputNumber
 }
 
+function addAmount(totalId, amount) {
+    const previousTotal = document.getElementById(totalId);
+    const previousTotalNum = parseFloat(previousTotal.innerText);
+    previousTotal.innerText = amount + previousTotalNum;
+}
+
+function balanceAmount() {
+    const previousBalanceText = document.getElementById('balance-total');
+    const previousBalance = parseFloat(previousBalanceText.innerText);
+    return previousBalance;
+}
+
+function balance(amount, isAdd) {
+    const previousBalanceText = document.getElementById('balance-total');
+    const previousBalance = balanceAmount();
+    if (isAdd == true) {
+        previousBalanceText.innerText = previousBalance + amount;
+    } else {
+        previousBalanceText.innerText = previousBalance - amount;
+    }
+}
 // deposit Money
 document.getElementById('button-deposit').addEventListener('click', function () {
-    const deposit = getInputValue('input-deposit')
-
-
-    const previousDeposit = parseFloat(document.getElementById('deposit-total').innerText);
-    const totalDeposit = deposit + previousDeposit;
-    document.getElementById('deposit-total').innerText = totalDeposit
-
-    const previousBalance = parseFloat(document.getElementById('balance-total').innerText);
-    const totalBalance = deposit + previousBalance;
-    document.getElementById('balance-total').innerText = totalBalance;
-
+    const deposit = getInputValue('input-deposit');
+    if (deposit > 0) {
+        addAmount('deposit-total', deposit);
+        balance(deposit, true);
+    }
 })
 // Withdraw Money
 document.getElementById('button-withdraw').addEventListener('click', function () {
-    const withdraw = getInputValue('input-withdraw')
-
-
-    const previousWithdraw = parseFloat(document.getElementById('withdraw-total').innerText);
-    const totalWithdraw = withdraw + previousWithdraw;
-    document.getElementById('withdraw-total').innerText = totalWithdraw;
-    const previousBalance = parseFloat(document.getElementById('balance-total').innerText);
-    const totalBalance = previousBalance - withdraw;
-    document.getElementById('balance-total').innerText = totalBalance;
-
+    const withdraw = getInputValue('input-withdraw');
+    const previousBalance = balanceAmount();
+    if (withdraw > 0 && previousBalance > 0) {
+        addAmount('withdraw-total', withdraw);
+        balance(withdraw, false);
+    }
 })
